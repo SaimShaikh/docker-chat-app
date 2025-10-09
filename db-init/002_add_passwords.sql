8d7cda9ca1a717ab1042a8cdd24bbb2be11d7fdc
saim <103352600+SaimShaikh@users.noreply.github.com>
2025-10-09 15:31:31 +0530

    Delete db-init/002_add_passwords.sql

--- db-init/002_add_passwords.sql
+++
@@ -1,5 +0,0 @@
--- Modified by Muhammad Abdulghaffar
--- Adds a password column (if missing). App will hash '123' at startup.
-
-ALTER TABLE users
-  ADD COLUMN IF NOT EXISTS password VARCHAR(255) NULL;
